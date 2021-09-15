710aa00aec8d6736a424a40492ab00fcc5f9f179
Daniel Meireles <daniel@inovadora.com.br>
2021-09-14 22:13:52 -0300

    Separando a funcao que monta o li

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,7 @@
  * Função para Trazer a lista de tarefas que esta quardada no localStorage ou um array vazio.
  * @returns {[]}
  */
-function getTasks(){
+function getTasks() {
     return JSON.parse(localStorage.getItem('tasks')) || []
 }
 /**
@@ -11,49 +11,63 @@ function getTasks(){
  * @param {array} tasks 
  * @returns 
  */
-function loadTasksToView(tasks){
+function loadTasksToView(tasks) {
     const ulTasks = document.querySelector('#taskList')
     ulTasks.innerHTML = ''
-    if(tasks.length === 0 ){
+    if (tasks.length === 0) {
         ulTasks.classList.add('empty')
         ulTasks.innerHTML = '<p>Lista de Tarefas vazia.</p>'
         return
     }
-    if(ulTasks.classList.contains('empty')){
+    if (ulTasks.classList.contains('empty')) {
         ulTasks.classList.remove('empty')
     }
-    tasks.forEach((item, i)=>{
-        const liItem = createElementTasks('li')
-        const pItem = createElementTasks('p')
-        const imgItemCheck = createElementTasks('img', 'check')
-        const imgItemDelete = createElementTasks('img', 'delete')
-        const imgItemEditar = createElementTasks('img', 'edit')
-        imgItemDelete.setAttribute('onclick', `deleteItem(${i})`)
-        imgItemDelete.setAttribute('title', 'Deletar Item')
-        imgItemEditar.setAttribute('onclick', `editarItem(${i})`)
-        imgItemEditar.setAttribute('title', 'Editar Item')
-        pItem.setAttribute('onclick', `markAsDone(${i})`)
-        pItem.setAttribute('title', 'Marcar tarefa')
-        imgItemCheck.setAttribute('onclick', `markAsDone(${i})`)
-        if(item.isDone) {
-            pItem.classList.add('done')
-            imgItemCheck.setAttribute('src', 'assets/img/done.svg')
-        }
-        pItem.innerHTML = item.description
-        liItem.appendChild(imgItemCheck)
-        liItem.appendChild(pItem)
-        liItem.appendChild(imgItemEditar)
-        liItem.appendChild(imgItemDelete)
-        ulTasks.appendChild(liItem)
+    tasks.forEach((item, i) => {
+        const li = makeItem(item, i)
+        ulTasks.appendChild(li)
     })
+}
+/**
+ * Função para montar o li com os dados da task, recebe o item e o indice do item
+ * @param {Object} item 
+ * @param {Number} i 
+ * @returns 
+ */
+function makeItem(item, i) {
+    const liItem = createElementTasks('li')
+    const pItem = createElementTasks('p')
+    const imgItemCheck = createElementTasks('img', 'check')
+    const imgItemDelete = createElementTasks('img', 'delete')
+    const imgItemEditar = createElementTasks('img', 'edit')
+
+    imgItemDelete.setAttribute('onclick', `deleteItem(${i})`)
+    imgItemDelete.setAttribute('title', 'Deletar Item')
+    imgItemEditar.setAttribute('onclick', `editarItem(${i})`)
+    imgItemEditar.setAttribute('title', 'Editar Item')
+    pItem.setAttribute('onclick', `markAsDone(${i})`)
+    pItem.setAttribute('title', 'Marcar Tarefa como Concluida')
+    imgItemCheck.setAttribute('title', 'Marcar Tarefa como Concluida')
+    imgItemCheck.setAttribute('onclick', `markAsDone(${i})`)
+    if (item.isDone) {
+        pItem.classList.add('done')
+        pItem.setAttribute('title', 'Desmarcar Tarefa')
+        imgItemCheck.setAttribute('src', 'assets/img/done.svg')
+        imgItemCheck.setAttribute('title', 'Desmarcar Tarefa')
+    }
+    pItem.innerHTML = item.description
+    liItem.appendChild(imgItemCheck)
+    liItem.appendChild(pItem)
+    liItem.appendChild(imgItemEditar)
+    liItem.appendChild(imgItemDelete)
+    return liItem
 }
 /**
  * Função para editar um item, recebe a posição do item no array de tasks, remove o item do array e inseri o texto no input para ser editado.
  * @param {Number} id 
  * @returns 
  */
-function editarItem(id){
-    if (document.querySelector('#task').getAttribute('editando')){
+function editarItem(id) {
+    if (document.querySelector('#task').getAttribute('editando')) {
         showAlert('Termine de editar o item antes de fazer outra ação.', 'error')
         document.querySelector('#task').focus()
         return
@@ -73,8 +87,8 @@ function editarItem(id){
  * @param {Number} id 
  * @returns 
  */
-function deleteItem(id){
-    if (document.querySelector('#task').getAttribute('editando')){
+function deleteItem(id) {
+    if (document.querySelector('#task').getAttribute('editando')) {
         showAlert('Termine de editar o item antes de fazer outra ação.', 'error')
         document.querySelector('#task').focus()
         return
@@ -88,12 +102,12 @@ function deleteItem(id){
  * Função para marcar como concluido ou desmarcar, recebe a posição do item no array muda o atributo isDone e salva novamente.
  * @param {Number} id 
  */
-function markAsDone(id){
+function markAsDone(id) {
     const tasks = getTasks()
-    if(tasks[id].isDone) {
+    if (tasks[id].isDone) {
         tasks[id].isDone = false
         showAlert('Não Concluida.', 'success', 5000)
-    } else {        
+    } else {
         tasks[id].isDone = true
         showAlert('Concluida.', 'success', 5000)
     }
@@ -109,10 +123,10 @@ function markAsDone(id){
  */
 function createElementTasks(tag, classElement = '') {
     const element = document.createElement(tag)
-    if(classElement == '') {
+    if (classElement == '') {
         return element
     }
-    if(tag == 'img') {
+    if (tag == 'img') {
         element.setAttribute('src', `assets/img/${classElement}.svg`)
     }
     element.classList.add(classElement)
@@ -143,12 +157,12 @@ function saveTasks(tasks = []) {
 function showAlert(message, classMessage = 'success', time = 5000) {
     const divAlert = document.querySelector('#alert')
     divAlert.classList.add(classMessage)
-    if(message === '') {
+    if (message === '') {
         message = 'Erro: Algum erro ocorreu.'
     }
-    divAlert. innerHTML = message
-    setTimeout(function(){
+    divAlert.innerHTML = message
+    setTimeout(function () {
         divAlert.innerHTML = ''
         divAlert.classList.remove(classMessage)
-    }, time)    
+    }, time)
 }
